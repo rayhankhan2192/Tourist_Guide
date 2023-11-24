e2d069137b75ded39c78f2e338de1cd827b6e246
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2023-11-24 23:14:06 +0600

    Implement Spring Security

---
+++ TOURIST GUIDE/travelGuide/src/main/java/com/TouristNest/travelGuide/Config/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.TouristNestApplication.TravelGuide.Config;
+
+import com.TouristNestApplication.TravelGuide.JPArepository.UserDataRepository;
+import com.TouristNestApplication.TravelGuide.Model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserDataRepository userDataRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userDataRepository.findByEmail(email);
+        if (Objects.equals(user, null)) {
+            throw new UsernameNotFoundException("Username not found!");
+        } else {
+            return new CustomUser(user);
+        }
+    }
+
+}
--- TOURIST GUIDE/travelGuide/src/main/java/com/TouristNest/travelGuide/Config/SecurityConfig.java
+++ TOURIST GUIDE/travelGuide/src/main/java/com/TouristNest/travelGuide/Config/SecurityConfig.java
@@ -1,11 +1,75 @@
-package com.TouristNest.travelGuide.Config;
+package com.TouristNestApplication.TravelGuide.Config;
 
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 
 @Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter{
-//}
-public class SecurityConfig{
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    @Bean
+    public UserDetailsService getDetailsService(){
+        return new CustomUserDetailsService();
+    }
+
+
+    @Bean
+    public DaoAuthenticationProvider getAuthenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(getDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(@NotNull HttpSecurity httpSecurity)throws Exception{
+        httpSecurity
+                .csrf().disable()
+                    .authorizeHttpRequests()
+                    .requestMatchers("/touristNest/berlin","/css/**","/Berlin/**").permitAll()
+                    .requestMatchers("/touristNest/**","/css/**","/image/**").permitAll()
+                    .requestMatchers("/touristNest/signup/**").permitAll()
+                    .requestMatchers("/hotel/**", "/css/**","/Berlin/**").permitAll()
+                    .requestMatchers("/SpittelmarktBerlin/**").permitAll()
+                .requestMatchers("/otherEndpoint").permitAll()
+                .requestMatchers("/user/**").authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/signin")
+                    .loginProcessingUrl("/login")
+                    .usernameParameter("email")
+                .defaultSuccessUrl("/user/profile").permitAll()
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutSuccessHandler(logoutSuccessHandler())
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID");
+        return  httpSecurity.build();
+    }
+
+    @Bean
+    public LogoutSuccessHandler logoutSuccessHandler() {
+        SimpleUrlLogoutSuccessHandler successHandler = new SimpleUrlLogoutSuccessHandler();
+        successHandler.setUseReferer(true); // Redirect to the referer page after logout
+        return successHandler;
+    }
 
 }
